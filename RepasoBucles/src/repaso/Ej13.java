5029db931c8c1ba70153294ebb9219451c389765
Siobhantt <luisa.cardozo@iesnervion.es>
2023-01-17 08:26:33 +0100

    por hacer el 13

--- RepasoBucles/src/repaso/Ej13.java
+++ RepasoBucles/src/repaso/Ej13.java
@@ -1,7 +1,9 @@
 package repaso;
 
 public class Ej13 {
-
+/*Realiza un programa donde el usuario "piensa" un número del 1 al 100 y el ordenador intenta
+adivinarlo. Es decir, el ordenador irá proponiendo números una y otra vez hasta adivinarlo (el
+usuario deberá indicarle al ordenador si es mayor, menor o igual al número que ha pensado).*/
 	public static void main(String[] args) {
 		
 
